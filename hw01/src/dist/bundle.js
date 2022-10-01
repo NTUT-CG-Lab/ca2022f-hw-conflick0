911801a9b0b0dce017d0729320377b26e18f7545
conflick0 <jackhus0099@gmail.com>
2022-10-01 11:03:36 +0800

    add hw01 meow

--- hw01/src/dist/bundle.js
+++ hw01/src/dist/bundle.js
@@ -9429,7 +9429,7 @@ exports.BackImageName = 'back_class_normal.png';
 exports.GearImageName = 'icon_gear.png';
 exports.PowerImageName = 'CloseNormal.png';
 exports.ModelDir = [
-    'miku_sample_t04'
+    'Cat'
 ];
 exports.ModelDirSize = exports.ModelDir.length;
 exports.MotionGroupIdle = 'Idle';
